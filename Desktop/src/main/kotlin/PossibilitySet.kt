af325fbf42d3e6a434f7b057e79d9e2255d19f7f
Matthew Weser <matthew.weser@gmail.com>
2018-06-10 18:47:34 -0400

    Create elimination method

---
+++ Desktop/src/main/kotlin/PossibilitySet.kt
@@ -0,0 +1,17 @@
+class PossibilitySet(val cellValue: CellValue = CellValue.EMPTY) {
+
+    var array = ArrayList<CellValue>()
+    var prevArray = ArrayList<CellValue>()
+
+    fun eliminateValue(cellValue: CellValue) {
+        array.remove(cellValue)
+    }
+
+    fun populateWithAllExceptCellValue() {
+        for (index in 1 until 10) {
+            if (cellValue.ordinal != index) {
+                array.add(CellValue.values()[index])
+            }
+        }
+    }
+}
