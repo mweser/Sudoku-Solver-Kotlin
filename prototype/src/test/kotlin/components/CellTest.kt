d9eef833b4b62f921259277267c99f0a8bca50b5
Matthew Weser <matthew.weser@gmail.com>
2018-06-14 10:15:21 -0400

    Fix up gradle and start unit test

--- prototype/src/test/kotlin/components/CellTest.kt
+++ prototype/src/test/kotlin/components/CellTest.kt
@@ -1,50 +1,53 @@
-//package components
-//
-//import components.CellValue.NONE
-//import org.junit.Before
-//import org.junit.Test
-//import org.mockito.Mockito.`when`
-//import org.mockito.Mockito.mock
-//
-//class CellTest {
-//
-//    lateinit var cell: Cell
-//
-//    var row = mock(Row::class.java)
-//    var column = mock(Column::class.java)
-//    var block = mock(Block::class.java)
-//
-//    @Before
-//    fun setUp() {
-//
-//        `when`(row.values).thenReturn(populateMockWithValues(0, 1, 2, 3, 4, 0, 0, 7, 8, 9))
-//        `when`(column.values).thenReturn(populateMockWithValues(0, 0, 2, 0, 4, 0, 6, 0, 8, 0))
-//        `when`(block.values).thenReturn(populateMockWithValues(0, 0, 2, 0, 4, 0, 0, 0, 0, 0))
-//
-//        cell = Cell(row = row, column = column, block = block, index = 1)
-//    }
-//
-//    /**
-//     * Create mock rows, columns, and blocks and pass them into Cell
-//     * Run .eliminate()
-//     * Check values on Cell
-//     */
-//    @Test
-//    fun testEliminateOneValueRemaining() {
-//
-//        //cell.eliminate()
-//
-//        assert(true)
-//
-//    }
-//
-//    private fun populateMockWithValues(vararg intValues: Int): Array<CellValue> {
-//        var valueArray = Array<CellValue>(0) { NONE }
-//
-//        for (i in 0 until 9) {
-//            valueArray[i] = CellValue.values()[intValues[i]]
-//        }
-//
-//        return valueArray
-//    }
-//}
+package components
+
+import com.nhaarman.mockito_kotlin.whenever
+import components.CellValue.NONE
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class CellTest {
+
+    lateinit var cell: Cell
+
+    var row = mock(Row::class.java)
+    var column = mock(Column::class.java)
+    var block = mock(Block::class.java)
+    var cells = ArrayList<mock(Cell)
+
+    @Before
+    fun setUp() {
+
+
+
+        whenever(row.values).thenReturn(populateMockWithValues(0, 1, 2, 3, 4, 0, 0, 7, 8, 9))
+        whenever(column.values).thenReturn(populateMockWithValues(0, 0, 2, 0, 4, 0, 6, 0, 8, 0))
+        whenever(block.values).thenReturn(populateMockWithValues(0, 0, 2, 0, 4, 0, 0, 0, 0, 0))
+
+        cell = Cell(row = row, column = column, block = block, index = 1)
+    }
+
+    /**
+     * Create mock rows, columns, and blocks and pass them into Cell
+     * Run .eliminate()
+     * Check values on Cell
+     */
+    @Test
+    fun testEliminateOneValueRemaining() {
+
+        //cell.eliminate()
+
+        assert(true)
+
+    }
+
+    private fun populateMockWithValues(vararg intValues: Int): Array<CellValue> {
+        var valueArray = Array(0) { NONE }
+
+        for (i in 0 until 9) {
+            valueArray[i] = CellValue.values()[intValues[i]]
+        }
+
+        return valueArray
+    }
+}
