7b4f5d989934e685b17296a076f193be8ada3802
Matthew Weser <matthew.weser@gmail.com>
2018-06-17 18:12:04 -0400

    Reorganize unit testing structure

---
+++ prototype/src/test/kotlin/solver/rules/SingleNakedTest.kt
@@ -0,0 +1,18 @@
+package solver.rules
+
+import org.junit.Before
+import org.junit.Test
+
+class SingleNakedTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun check() {
+
+
+
+    }
+}
