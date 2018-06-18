ecb2b2ade92b45cf5f97f11ed1b61f0c109f0e80
Matthew Weser <matthew.weser@gmail.com>
2018-06-18 09:02:29 -0400

    Create unit tests to start with for hidden single

--- prototype/src/test/kotlin/solver/SingleHiddenTest.kt
+++ prototype/src/test/kotlin/solver/SingleHiddenTest.kt
@@ -12,4 +12,68 @@ class SingleHiddenTest {
     @Test
     fun check() {
     }
+
+    @Test
+    fun testOtherCellSameAsCurrentCell() {
+
+
+    }
+
+    @Test
+    fun testCandidateNotInOtherCell() {
+
+
+    }
+
+    @Test
+    fun testCandidateInOtherCell() {
+
+
+    }
+
+    @Test
+    fun testCheckWithHiddenSingleInRow() {
+
+
+    }
+    @Test
+    fun testCheckWithHiddenSingleInColumn() {
+
+
+    }
+
+    @Test
+    fun testCheckWithHiddenSingleInBlock() {
+
+
+    }
+
+    @Test
+    fun testCheckWithSameHiddenSingleInRow() {
+
+
+    }
+
+    @Test
+    fun testCheckWithHiddenSingleInMultiplePlaces() {
+
+
+    }
+
+    @Test
+    fun testCheckWithHiddenSingleInNone() {
+
+
+    }
+
+    @Test
+    fun testCheckWithHiddenSingleInAllThreeRowTypes() {
+
+
+    }
+
+
+
+
+
 }
