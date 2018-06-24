6cd4f3bc5f461fccac17088e0dc10b74e46bee91
Matthew Weser <matthew.weser@gmail.com>
2018-06-23 23:43:46 -0400

    Start Hidden Double checks

--- prototype/src/main/kotlin/solver/DoubleHidden.kt
+++ prototype/src/main/kotlin/solver/DoubleHidden.kt
@@ -1,2 +1,17 @@
 package solver
 
+object DoubleHidden : RuleCheck() {
+
+    var count = 0
+
+    override fun check(): Int {
+        count = 0
+
+        
+
+
+//        Logger.printNakedDoublesResults(this)
+        return count
+    }
+
+}
