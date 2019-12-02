9e4af9b5cc7ae563b8c1a872e772ced90af924c4
Matthew Weser <matthew.weser@gmail.com>
2019-12-02 18:48:18 -0500

    Add check for correct puzzle solution

--- prototype/src/main/kotlin/Main.kt
+++ prototype/src/main/kotlin/Main.kt
@@ -6,13 +6,14 @@ import solver.LockedCandidateClaiming
 import solver.LockedCandidatePointing
 import solver.SingleHidden
 import solver.SingleNaked
+import util.FileInput.importFileToPuzzleArray
 import util.Logger.printRoundNumberAndCandidateTable
 
 fun main(args: Array<String>) {
     var counter = 0
     var isDone = false
 
-    Table.populateCellsWithValues("test.txt")
+    Table.populateCellsWithValues("hard01.txt")
     Eliminate.check()
 
     while (counter < 30 && !isDone) {
@@ -22,6 +23,31 @@ fun main(args: Array<String>) {
     }
 
     printRoundNumberAndCandidateTable(counter++, Table)
+
+    checkSolution("hard01.txt.soln")
+
+    println("Done")
+}
+
+fun checkSolution(inputFile: String) {
+    var solutionTable = importFileToPuzzleArray(inputFile)
+    var hasMismatch = false
+
+
+    for(index in 0 until 81) {
+
+        var tableValue = Table.cells.get(index).value.ordinal
+        var solutionValue = solutionTable[index]
+
+        if (tableValue != solutionValue) {
+            println("ERROR: Mismatch for index $index. Expected: $solutionValue, Actual: $tableValue")
+            hasMismatch = true
+        }
+
+    }
+    if(!hasMismatch) {
+        println("Puzzle is correct")
+    }
 }
 
 fun solve(counter: Int): Boolean {
